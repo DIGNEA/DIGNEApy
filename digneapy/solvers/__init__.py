0866bc601c289e8f6156ea63937d8e841dc82bfb
amarrerod <alemarrerodiaz@gmail.com>
2023-11-02 12:09:21 +0000

    🐛 Fixes a bug in the solvers mod.

---
+++ digneapy/solvers/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2023/11/02 12:06:59
+@Author  :   Alejandro Marrero 
+@Version :   1.0
+@Contact :   amarrerd@ull.edu.es
+@License :   (C)Copyright 2023, Alejandro Marrero
+@Desc    :   None
+"""

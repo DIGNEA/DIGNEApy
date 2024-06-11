802f27023936902bbed925fb62e9d6555db740a2
amarrerod <alemarrerodiaz@gmail.com>
2024-06-11 10:50:36 +0100

    🚧 Working on GridArchive

--- digneapy/archives/_grid_archive.py
+++ digneapy/archives/_grid_archive.py
@@ -13,8 +13,6 @@
 from ._base_archive import Archive
 from digneapy.core import Instance
 import numpy as np
-import operator
-import reprlib
 from collections.abc import Iterable, Sequence
 from functools import reduce
 from typing import Optional
@@ -25,11 +23,17 @@ class GridArchive(Archive):
         self,
         dimensions: Sequence[int],
         ranges: Sequence[float],
-        lr: float,
         instances: Optional[Iterable[Instance]] = None,
     ):
-        Archive.__init__(self, instances)
+        Archive.__init__(self, threshold=np.inf)
         self._dimensions = tuple(dimensions)
         self._lower_bounds = np.array(ranges[0], dtype=np.float64)
         self._upper_bounds = np.array(ranges[1], dtype=np.float64)
         self._interval_size = self._upper_bounds - self._lower_bounds
+        self._bins = np.prod(self._dimensions)
+
+    def __str__(self):
+        return f"GridArchive(dim={self._dimensions},bins={self._bins},bounds=[{self._lower_bounds[0]},{self._upper_bounds[0]}])"
+
+    def __repr__(self):
+        return f"GridArchive(dim={self._dimensions},bins={self._bins},bounds=[{self._lower_bounds[0]},{self._upper_bounds[0]}])"

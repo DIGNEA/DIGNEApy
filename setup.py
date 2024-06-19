f10da1ead456d871a1de2b66a1252221395bc047
amarrerod <alemarrerodiaz@gmail.com>
2024-06-19 08:08:21 +0100

    ➖ Using Self requires Python>=3.11

--- setup.py
+++ setup.py
@@ -66,14 +66,13 @@ ext_modules = [
 setup(
     author="Alejandro Marrero",
     author_email="amarrerd@ull.edu.es",
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
     ],

58123306607235cc1f171e18b7e3199b61126b8e
amarrerod <alemarrerodiaz@gmail.com>
2024-09-11 14:45:06 +0100

    💚 Includes logic to build pybind11 extensions

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from glob import glob
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked."""
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+
+        return pybind11.get_include(self.user)
+
+
+compile_args = ["-std=c++11"]
+ext_modules = [
+    Extension(
+        "pisinger_cpp",
+        sorted(glob("digneapy/solvers/pisinger/src/*.cpp")),
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True),
+        ],
+        extra_compile_args=compile_args,
+        language="c++",
+    ),
+    Extension(
+        "parallel_ea",
+        sorted(glob("digneapy/solvers/parallel_ea/src/*.cpp")),
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True),
+        ],
+        extra_compile_args=compile_args,
+        language="c++",
+    ),
+]
+
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)

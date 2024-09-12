60bdc6ca1ffe63ef5a3de7a96ad111c8599cf0e2
amarrerod <alemarrerodiaz@gmail.com>
2024-09-12 13:29:59 +0100

    🚨 Removes warning management from tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,1 @@
 """Unit test package for digneapy."""
-
-import warnings
-
-from sklearn.exceptions import InconsistentVersionWarning
-
-warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
-warnings.filterwarnings("ignore", category=DeprecationWarning)

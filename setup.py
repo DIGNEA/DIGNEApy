5e115cc9f3ee8e2d465c89db22e4a5c88d58bb00
amarrerod <alemarrerodiaz@gmail.com>
2024-09-23 15:23:14 +0100

    👷 Updates setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,20 @@
 
 from glob import glob
 
-from setuptools import Extension, setup
+from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.md") as history_file:
+    history = history_file.read()
+
+requirements = []
+
+test_requirements = [
+    "pytest>=3",
+]
 
 
 class get_pybind_include(object):
@@ -51,6 +63,44 @@ ext_modules = [
 ]
 
 setup(
+    author="Alejandro Marrero",
+    author_email="amarrerd@ull.edu.es",
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering",
+        "Typing :: Typed",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+    description="Python version of the DIGNEA code for instance generation",
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords=[
+        "dignea",
+        "optimization",
+        "instance generation",
+        "quality-diversity",
+        "NS",
+    ],
+    name="digneapy",
+    packages=find_packages(include=["digneapy", "digneapy.*"]),
+    platforms=["any"],
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/DIGNEA/digneapy",
+    version="0.2.4",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
+    zip_safe=False,
 )
